75acd2c1b15ac214f7697919e2fc2b85197cb243
DamaniN <37876601+DamaniN@users.noreply.github.com>
2024-03-10 23:31:43 -0700

    Added support for launching private tunnel.

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,22 @@
+output "cluster_ca_certificate" {
+  value = "${base64decode(aws_eks_cluster.rsc_exocompute.certificate_authority.0.data)}"
+}
+
+output "cluster_connection_command" {
+  value = polaris_aws_exocompute_cluster_attachment.cluster.connection_command
+}
+
+output "cluster_endpoint" {
+  value = aws_eks_cluster.rsc_exocompute.endpoint
+} 
+
+output "cluster_name" {
+  value = var.aws_eks_cluster_name
+}
+
+output "cluster_token" {
+  value = data.aws_eks_cluster_auth.rsc_exocompute.token
+}
 
 # Output the ssh private key for the worker nodes when local login to nodes is enabled.
 output "worker_ssh_private_key" {
